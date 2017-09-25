0ae096b702c7590402e0ad4fff7be37bd12f341f
Rémi Flamary <remi.flamary@gmail.com>
2017-09-25 10:32:01 +0200

    figure paper final version

--- visu_regression.py
+++ visu_regression.py
@@ -26,35 +26,34 @@ np.random.seed(seed)
 
 n = 200
 ntest=200
-nz=.3
-
-theta=0.8
 
 
-n2=int(n/2)
-sigma=0.05
+def get_data(n,ntest):
 
-xs=np.random.randn(n,1)+2
-xs[:n2,:]-=4
-ys=sigma*np.random.randn(n,1)+np.sin(xs/2)
+    n2=int(n/2)
+    sigma=0.05
+    
+    xs=np.random.randn(n,1)+2
+    xs[:n2,:]-=4
+    ys=sigma*np.random.randn(n,1)+np.sin(xs/2)
+    
+    
+    
+    xt=np.random.randn(n,1)+2
+    xt[:n2,:]/=2 
+    xt[:n2,:]-=3
+      
+    yt=sigma*np.random.randn(n,1)+np.sin(xt/2)
+    xt+=2
+    
+    return xs,ys,xt,yt
+
+xs,ys,xt,yt=get_data(n,ntest)
+
 
 fs_s = lambda x: np.sin(x/2)
-
-xt=np.random.randn(n,1)+2
-xt[:n2,:]/=2 
-xt[:n2,:]-=3
-  
-gauss = lambda x,s,m: np.exp((x-m)**2/(2*s*s))/(s*np.sqrt(2*np.pi))
-
-mus_x = lambda x: gauss(x,1,2)/2+gauss(x,1,-2)/2
-
-
-yt=sigma*np.random.randn(n,1)+np.sin(xt/2)
-xt+=2
-
 fs_t = lambda x: np.sin((x-2)/2)
 
-mut_x = lambda x: gauss(x,1,2)/2+gauss(x,1./2,-4)/2
                        
 xvisu=np.linspace(-4,6.5,100)
 
@@ -185,175 +184,152 @@ pl.title('Model estimated with JDOT',fontsize=fs)
 pl.tight_layout()
 #pl.savefig('imgs/visu_reg.eps')
 
-#%% visu joint distributions
-from matplotlib import gridspec
+#%% compute histograms
+
 seed=1985
 np.random.seed(seed)
 
-n = 2000000
-ntest=2000000
-nz=.3
+n2 = 2000000
+ntest2=2000000
 
-theta=0.8
-
-
-n2=int(n/2)
-sigma=0.05
-
-xs=np.random.randn(n,1)+2
-xs[:n2,:]-=4
-ys=sigma*np.random.randn(n,1)+np.sin(xs/2)
-
-fs_s = lambda x: np.sin(x/2)
-
-xt=np.random.randn(n,1)+2
-xt[:n2,:]/=2 
-xt[:n2,:]-=3
-  
-gauss = lambda x,s,m: np.exp((x-m)**2/(2*s*s))/(s*np.sqrt(2*np.pi))
-
-mus_x = lambda x: gauss(x,1,2)/2+gauss(x,1,-2)/2
-
-
-yt=sigma*np.random.randn(n,1)+np.sin(xt/2)
-xt+=2
-
-fs_t = lambda x: np.sin((x-2)/2)
-
-mut_x = lambda x: gauss(x,1,2)/2+gauss(x,1./2,-4)/2
+xs2,ys2,xt2,yt2=get_data(n2,ntest2)
                        
 xvisu=np.linspace(-4,6.5,100)
-
-#%% data visu
-pl.figure(5,(10,10))
-pl.clf()
-from matplotlib import gridspec
-gs = gridspec.GridSpec(3, 3)
-ax1 = pl.subplot(gs[0, 1:])
-
-#pl.hist(xs.ravel(),bins=200,histtype='step')
-#pl.hist(xt.ravel(),bins=200,histtype='step')
-
 nbin=100
+hys,pos=np.histogram(ys2,nbin)
+hyt,pot=np.histogram(yt2,nbin)
+
+hxs,posx=np.histogram(xs2,nbin)
+hxt,potx=np.histogram(xt2,nbin)
+
+
+#%%
+
 nbvisu=500
 
 iss=np.random.permutation(n)[:nbvisu]
 ist=np.random.permutation(ntest)[:nbvisu]
+#
 
-hys,pos=np.histogram(xs,nbin)
-hyt,pot=np.histogram(xt,nbin)
-pl.plot(pos[1:],hys)
-pl.plot(pot[1:],hyt)
+pl.figure(1)
+pl.clf()
 
-pl.yticks(())
-pl.ylim([0,pl.ylim()[1]])
-
-
-
-xl=pl.xlim()
-pl.title('x marginals')
-
-ax2 = pl.subplot(gs[1:, 0])
-
-#pl.hist(ys.ravel(),bins=200,histtype='step')
-#pl.hist(yt.ravel(),bins=200,histtype='step')
-pl.gca().invert_xaxis()
-#pl.gca().invert_yaxis()
-
-hys,pos=np.histogram(ys,nbin)
-hyt,pot=np.histogram(yt,nbin)
-pl.plot(hys,pos[1:])
-pl.plot(hyt,pot[1:])
-
-pl.xticks(())
-yl=pl.ylim()
-pl.xlim([pl.xlim()[0],0])
-rg=[xl, np.sort(yl)]
-pl.ylabel('y marginals')
-
-
-pl.subplot(gs[1:, 1:], sharex=ax1, sharey=ax2)
+ym=-8
+xm=-2
 
 pl.scatter(xs[iss],ys[iss],edgecolors='k')
 pl.scatter(xt[ist],yt[ist],edgecolors='k')
 pl.plot(xvisu,fs_s(xvisu),label='Source model')
 pl.plot(xvisu,fs_t(xvisu),label='Target model')
-#pl.hist2d(xs.ravel(),ys.ravel(),cmap='Blues',bins=200,alpha=0.99,range=rg)
-#pl.hist2d(xt.ravel(),yt.ravel(),cmap='Oranges',bins=200,alpha=0.7,range=rg)
+pl.xlim([ym,8])
+pl.ylim([xm,1.5])
 
-pl.xticks(())
-pl.yticks(())
+sx=2.0/hys.max()
+
+
+pl.fill(hyt*sx+ym,pot[1:],color='C1',alpha=0.5)
+pl.fill(hys*sx+ym,pos[1:],color='C0',alpha=0.5)
+
+sx2=0.4/hxs.max()
+
+
+pl.fill(potx[1:],hxt*sx2+xm,color='C1',alpha=0.5)
+pl.fill(posx[1:],hxs*sx2+xm,color='C0',alpha=0.5)
+
+pl.title("Joint distributions, model and marginals")
 pl.savefig('imgs/visu_data_reg.png')
-#
-#gs = gridspe
-
-
 #%%
-#seed=1985
-#np.random.seed(seed)
-#idv=np.random.permutation(n)
-#fs=12
-#nb=15
-#pl.figure(3,(12,3))
-#pl.clf()
-#
-#pl.subplot(1,4,1)
-#pl.scatter(xs,ys,label='Source samples',edgecolors='k')
-#pl.scatter(xt,yt,label='Target samples',edgecolors='k')
-#pl.plot(xvisu,fs_s(xvisu),label='Source model')
-#pl.plot(xvisu,fs_t(xvisu),label='Target model')
-##pl.xlabel('x')
+seed=1985
+np.random.seed(seed)
+idv=np.random.permutation(n)
+fs=12
+nb=15
+pl.figure(3,(12,3))
+pl.clf()
+
+pl.subplot(1,4,1)
+
+
+
+ym=-8
+xm=-2
+
+pl.scatter(xs,ys,label='Source samples',edgecolors='k')
+pl.scatter(xt,yt,label='Target samples',edgecolors='k')
 #pl.xlabel('x')
+pl.xlabel('x')
+pl.ylabel('y')
+
+
+pl.xlim([ym,8])
+pl.ylim([xm,1.5])
+
+sx=2.0/hys.max()
+
+
+pl.fill(hyt*sx+ym,pot[1:],color='C1',alpha=0.5)
+pl.fill(hys*sx+ym,pos[1:],color='C0',alpha=0.5)
+
+sx2=0.4/hxs.max()
+
+
+pl.fill(potx[1:],hxt*sx2+xm,color='C1',alpha=0.5)
+pl.fill(posx[1:],hxs*sx2+xm,color='C0',alpha=0.5)
+
+
+#pl.legend(loc=4,fontsize=.7*fs)
+pl.title('Toy regression distributions',fontsize=fs)
+
+pl.subplot(1,4,2)
+pl.scatter(xs,ys,edgecolors='k',label='Source samples')
+pl.scatter(xt,yt,edgecolors='k',label='Target samples')
+pl.plot(xvisu,fs_s(xvisu),color='C0',label='Source model')
+pl.plot(xvisu,fs_t(xvisu),color='C1',label='Target model')
+
+
+#pl.xlabel('x')
+
 #pl.ylabel('y')
-#pl.legend(loc=4,fontsize=.7*fs)
-#pl.title('Toy regression example',fontsize=fs)
-#
-#pl.subplot(1,4,2)
-#pl.scatter(xs,ys,edgecolors='k')
-#pl.scatter(xt,yt,edgecolors='k')
-##pl.plot(xvisu,fs_s(xvisu),'b',label='Source model')
-##pl.plot(xvisu,fs_t(xvisu),'g',label='Target model')
-#for i in range(nb):
-#    idt=G0[idv[i],:].argmax()
-#    if not i:
-#        pl.plot([xs[idv[i]],xt[idt]],[ys[idv[i]],yt[idt]],'k',label='OT matrix link')
-#    else:
-#        pl.plot([xs[idv[i]],xt[idt]],[ys[idv[i]],yt[idt]],'k')
-#
-##pl.xlabel('x')
-#
-##pl.ylabel('y')
-#pl.legend(loc=4,fontsize=.7*fs)
-#pl.title('OT matrix on joint distribution',fontsize=fs)
-#pl.xlabel('x')
-#pl.subplot(1,4,3)
-#pl.scatter(xs,ys,edgecolors='k')
-#pl.scatter(xt,yt,edgecolors='k')
-##pl.plot(xvisu,fs_s(xvisu),'b',label='Source model')
-##pl.plot(xvisu,fs_t(xvisu),'g',label='Target model')
-#for i in range(nb):
-#    idt=G[idv[i],:].argmax()
-#    
-#    pl.plot([xs[idv[i]],xt[idt]],[ys[idv[i]],yt[idt]],'k')
-#
-#pl.xlabel('x')
-#
-##pl.ylabel('y')
-#pl.legend(loc=4)
-#pl.title('OT matrix of JDOT',fontsize=fs)
-#
-#pl.subplot(1,4,4)
-#pl.scatter(xs,ys,edgecolors='k')
-#pl.scatter(xt,yt,edgecolors='k')
-#pl.plot(xvisu,fs_s(xvisu),label='Source model')
-#pl.plot(xvisu,fs_t(xvisu),label='Target model')
-#pl.plot(xvisu,ypred,'g',label='JDOT model')
-#pl.xlabel('x')
-#
-##pl.ylabel('y')
-#pl.legend(loc=4,fontsize=.7*fs)
-#pl.title('Model estimated with JDOT',fontsize=fs)
-#
-#pl.tight_layout(pad=00,h_pad=0)
-##pl.savefig('imgs/visu_reg2.eps')
-##pl.savefig('imgs/visu_reg2.pdf')
+pl.legend(loc=4,fontsize=.7*fs)
+pl.title('Toy regression models',fontsize=fs)
+pl.xlabel('x')
+pl.subplot(1,4,3)
+pl.scatter(xs,ys,edgecolors='k')
+pl.scatter(xt,yt,edgecolors='k')
+#pl.plot(xvisu,fs_s(xvisu),'b',label='Source model')
+#pl.plot(xvisu,fs_t(xvisu),'g',label='Target model')
+for i in range(nb):
+    idt=G[idv[i],:].argmax()
+    if not i:
+        pl.plot([xs[idv[i]],xt[idt]],[ys[idv[i]],yt[idt]],color='C2',label='JDOT matrix link')
+    else:    
+        pl.plot([xs[idv[i]],xt[idt]],[ys[idv[i]],yt[idt]],color='C2')
+for i in range(nb):
+    idt=G0[idv[i],:].argmax()
+    if not i:
+        pl.plot([xs[idv[i]],xt[idt]],[ys[idv[i]],yt[idt]],'k--',label='OT matrix link')
+    else:
+        pl.plot([xs[idv[i]],xt[idt]],[ys[idv[i]],yt[idt]],'k--')
+
+
+pl.xlabel('x')
+
+#pl.ylabel('y')
+pl.legend(loc=4)
+pl.title('Joint OT matrices',fontsize=fs)
+
+pl.subplot(1,4,4)
+pl.scatter(xs,ys,edgecolors='k')
+pl.scatter(xt,yt,edgecolors='k')
+pl.plot(xvisu,fs_s(xvisu),label='Source model')
+pl.plot(xvisu,fs_t(xvisu),label='Target model')
+pl.plot(xvisu,ypred,'g',label='JDOT model')
+pl.xlabel('x')
+
+#pl.ylabel('y')
+pl.legend(loc=4,fontsize=.7*fs)
+pl.title('Model estimated with JDOT',fontsize=fs)
+
+pl.tight_layout(pad=00,h_pad=0)
+#pl.savefig('imgs/visu_reg2.eps')
+pl.savefig('imgs/visu_reg2.pdf')
